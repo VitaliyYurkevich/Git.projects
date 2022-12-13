817c0823ee0882941f0f8de6d9b7225a8476c524
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2022-12-14 00:50:31 +0300

    upd

---
+++ 11.12/test.js
@@ -0,0 +1,11 @@
+function quarterOf(month) {
+    if(month = 1, 2, 3) {
+    return 1
+    } if(month = 4, 5, 6) {
+    return 2
+    } if( month = 7, 8, 9) {
+    return 3
+    } if( month = 10, 11, 12 ){
+    return 4
+    }
+    }
