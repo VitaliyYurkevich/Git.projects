be396f4e202acdbb0d6fb6d97e4851886e5bce42
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2022-12-18 18:00:39 +0300

    upd

---
+++ 18.12/Training JS 8Conditional statementswitch8kyu.js
@@ -0,0 +1,33 @@
+function howManydays(month){
+    var days;
+    switch (month){
+    case 4:
+        case 6:
+        case 9:
+        case 11:
+        days = 30;
+        break;
+        case 2:
+        days = 28;
+        break;
+        default:
+        days = 31
+    }
+    return days;
+}
+
+
+
+
+
+/*//
+function howManydays(month){
+switch (month){
+    case 2: return 28
+    case 4:
+    case 6:
+    case 9:
+    case 11: return 30
+}
+return 31
+} */
