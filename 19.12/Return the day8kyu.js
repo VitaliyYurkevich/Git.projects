cb69fd46773370cbba3c47b35be2339708eebef0
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2022-12-19 01:11:39 +0300

    upd

---
+++ 19.12/Return the day8kyu.js
@@ -0,0 +1,38 @@
+function whatday(num) { 
+    if(num==1) return "Sunday"
+    if(num==2) return "Monday"
+    if(num==3) return "Tuesday"
+    if(num==4) return "Wednesday"
+    if(num==5) return "Thursday"
+    if(num==6) return "Friday"
+    if(num==7) return "Saturday"
+    else return "Wrong, please enter a number between 1 and 7"
+}
+
+
+
+
+
+/*
+||
+function whatday(num) { 
+switch(num) {
+    case 1:
+    return "Sunday";
+    case 2:
+    return "Monday";
+    case 3:
+    return "Tuesday";
+    case 4:
+    return "Wednesday";
+    case 5:
+    return "Thursday";
+    case 6:
+    return "Friday";
+    case 7:
+    return "Saturday";
+    default:
+    return 'Wrong, please enter a number between 1 and 7';
+}
+}
+*/
