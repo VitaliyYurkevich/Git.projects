233b16cfbfbdae0336b5dc5970779d57f8393383
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2022-12-24 23:24:09 +0300

    upd

--- test js/test2.js
+++ test js/test2.js
@@ -1,6 +1,8 @@
 
 let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51,69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44] 
-    function printAndGetHighScore(scores) {
+let costs = [.25, .27, .25, .25, .25, .25, .33, .31, .25, .29, .27, .22, .31, .25, .25, .33, .21, .25, .25, .25, .28, .25, .24, .22,.20, .25, .30, .25, .24, .25, .25, .25, .27, .25, .26, .29];
+let highScore, bestSolutions;
+function printAndGetHighScore(scores) {
         let highScore = 0;
         let output;
     for(let i = 0; i < scores.length; i++) {
@@ -22,13 +24,13 @@ let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44
     return bestSolutions;
     }
 }
-    let highScore = printAndGetHighScore(scores);
+    highScore = printAndGetHighScore(scores);
     console.log("Bubbles tests: " + scores.length);
     console.log("Highest bubble score: " + highScore);
 
-    let bestSolutions = getBestResults(scores, highScore);
+    bestSolutions = getBestResults(scores, highScore);
     console.log("Solutions with the highest score: " + bestSolutions);
-    var costs = [.25, .27, .25, .25, .25, .25, .33, .31, .25, .29, .27, .22, .31, .25, .25, .33, .21, .25, .25, .25, .28, .25, .24, .22,.20, .25, .30, .25, .24, .25, .25, .25, .27, .25, .26, .29];
+    
 function getMostCostEffectiveSolution(scores, costs, highScore) {
     let cost = 100;
     let index;
@@ -40,6 +42,7 @@ function getMostCostEffectiveSolution(scores, costs, highScore) {
                 }
             }
 } 
+return index
 }
 var mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
 console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
