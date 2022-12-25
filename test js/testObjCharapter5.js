17eed6c5fc320b7fc4fc568e716a7172e547cdfc
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2022-12-25 15:44:53 +0300

    upd

---
+++ test js/testObjCharapter5.js
@@ -0,0 +1,51 @@
+let taxi = { 
+    make: "Webville Motors",
+    model: "Taxi",
+    year: 1955,
+    color: "yellow",
+    passengers: 4,
+    convertible: false,
+    mileage: 281341
+};
+let cadi = {
+    make: 'GM',
+    model: 'Cadillac',
+    year: 1955,
+    color: 'tan',
+    passengers: 5,
+    convertible: false,
+    mileage: 12892
+}
+let fiat = {
+    make: 'Fiat',
+    model: '500',
+    year: 1957,
+    color: 'Medium Blue',
+    passengers: 2,
+    convertible: false,
+    mileage: 88000
+}
+let chevy = {
+    make: 'Chevy',
+    model: 'Bel Air',
+    year: 1957,
+    color: 'red',
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+}
+    function prequal(car) {
+    if (car.mileage > 10000) {
+    return false;
+    } else if (car.year > 1960) {
+    return false;
+    }
+    return true;
+}
+    let worthALook = prequal(chevy);
+    if (worthALook) {
+    console.log("You gotta check out this " + chevy.make + " " + chevy.model);
+    } else {
+    console.log("You should really pass on the " + chevy.make + " " + chevy.model);
+}
+
