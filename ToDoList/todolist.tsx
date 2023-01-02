9d3e342c36ba7a29ffb5ed5825f06560961b37e3
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-01-02 23:55:58 +0300

    upd

---
+++ ToDoList/todolist.tsx
@@ -0,0 +1,62 @@
+import React, {FC} from 'react';
+
+type TodoListPropsType = {
+    title: string
+    tasks: Array<TaskType>
+}
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+const TodoList: FC<TodoListPropsType> = (props) => {
+    // let tasksList;
+    // if(props.tasks.length === 0){
+    //     tasksList = <span>Your taskslist is empty</span>
+    // } else {
+    //     tasksList = props.tasks.map((task:TaskType) => {
+    //         return (
+    //             <li>
+    //                 <input type="checkbox" checked={task.isDone}/>
+    //                 <span>{task.title}</span>
+    //             </li>
+    //         )
+    //     })
+    // }
+
+
+    let tasksList = props.tasks.length
+    ? props.tasks.map((task:TaskType) => {
+            return (
+                <li>
+                    <input type="checkbox" checked={task.isDone}/>
+                    <span>{task.title}</span>
+                </li>
+            )
+        })
+    : <span>Your taskslist is empty</span>
+
+
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {tasksList}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+
+    );
+};
+
+export default TodoList;
